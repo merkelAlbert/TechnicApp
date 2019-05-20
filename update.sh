40f6c80f71a1890eb0565a7ebef5f7a5b7dec535
albert <merkel.albert1997@gmail.com>
2019-05-20 11:35:26 +0300

    script fix

--- update.sh
+++ update.sh
@@ -2,6 +2,8 @@
 git clone https://github.com/merkelAlbert/TechnicApp ../temp/
 rm -rf ../TechnicApp/
 mkdir ../TechnicApp
-mv ../temp/.* ../TechnicApp/
-mv ../temp/* ../TechnicApp/
+mv ../temp/.git ../TechnicApp
+mv ../temp/.gitignore ../TechnicApp
+mv ../temp/.dockerignore ../TechnicApp
+mv ../temp/* ../TechnicApp
 rm -rf ../temp/
