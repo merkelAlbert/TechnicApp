e3fefc6795f52206b6a841485ead5e34af2486e8
albert <merkel.albert1997@gmail.com>
2019-07-11 22:32:11 +0300

    fix

--- update.sh
+++ update.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-systemctl stop technic.service
-git clone https://github.com/merkelAlbert/TechnicApp ../temp/
-rm -rf ../TechnicApp/
-mkdir ../TechnicApp
-mv ../temp/.git ../TechnicApp
-mv ../temp/.gitignore ../TechnicApp
-mv ../temp/.dockerignore ../TechnicApp
-mv ../temp/* ../TechnicApp
-rm -rf ../temp/
+systemctl stop technic.service &&
+git clone https://github.com/merkelAlbert/TechnicApp ../temp/ &&
+rm -rf ../TechnicApp/ &&
+mkdir ../TechnicApp &&
+mv ../temp/.git ../TechnicApp &&
+mv ../temp/.gitignore ../TechnicApp &&
+mv ../temp/.dockerignore ../TechnicApp &&
+mv ../temp/* ../TechnicApp &&
+rm -rf ../temp/ &&
 systemctl start technic.service
 
 
